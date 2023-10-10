a8285c95f8dda83767f32f30b3a848f6e7667daf
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-10-10 15:28:56 +0200

    I am up to no good

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "pure-impure-nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , pre-commit-hooks
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      makePureImpure = drv: drv.overrideAttrs (old: 
+        let magicString = builtins.unsafeDiscardStringContext (builtins.substring 0 12 (baseNameOf drv.drvPath));
+            outputHashAlgo = "sha256";
+            outputHash = builtins.hashString outputHashAlgo magicString;
+        in {
+        preferHashedMirrors = false;
+        inherit outputHashAlgo;
+        inherit outputHash;
+        buildCommand = ''
+          ${old.buildCommand or ""}
+          rm -rf $out
+          echo -n "${magicString}" > $out
+        '';
+      });
+      testDerivation = pkgs.stdenv.mkDerivation {
+        name = "pure-impure-test";
+        dontUnpack = true;
+        buildInputs = [ pkgs.cacert pkgs.curl ];
+        buildCommand = ''
+          curl https://cs-syd.eu/foobar > $out
+        '';
+      };
+    in
+    {
+      checks.${system} = {
+        test = makePureImpure testDerivation;
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        name = "pure-impure-nix-shell";
+        buildInputs = (with pkgs; [
+        ]);
+      };
+    };
+}
