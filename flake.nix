c6fd35333cb8a1f62f89af9a12695bc98e9b79d4
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-10-10 16:17:16 +0200

    Explanation in the flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,37 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
+      # makePureImpure : Derivation -> Derivation
+      #
+      # This function turns a derivation that wants to access the internet (but
+      # fails because it tries to do so) into one that is allowed to access the
+      # internet but not produce any (useful) output.
+      #
+      # Note that this function (currently) only works for derivation produced
+      # with `mkDerivation` using `buildCommand`. PR Welcome.
+      #
+      # 
       # This is where the (evil?) magic happens.
+      #
+      # Important to know:
+      #
+      # * Nix lets derivations access the internet as long as they specify the
+      #   hash of the output. [citation needed]
+      # 
+      # * If you know the exact output of a derivation up front, you can
+      #   (pre-)compute the hash.
+      # 
+      # * We can know the exact output of a derivation up front if we replace
+      #   the actual output of a derivation by a magic string.
+      #
+      #
+      # Here's the plan:
+      #
+      # 1. We choose a magic string based on the store hash of the derivation.
+      # 2. We (pre-)compute the hash of this magic string in pure Nix.
+      # 3. We set the output hash of the derivation to this hash.
+      # 4. After 'building' the derivation, we remove the `$out` and replace it
+      #    by the magic string.
       makePureImpure = drv: drv.overrideAttrs (old:
         let
           magicString = builtins.unsafeDiscardStringContext (builtins.substring 0 12 (baseNameOf drv.drvPath));
